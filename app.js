d1a63654d1fef7d62043c85648b70d73b8e17740
Christyclear <YongYanLiang2018@gmail.com>
2020-09-20 18:00:19 -0400

    js videos

---
+++ app.js
@@ -0,0 +1,11 @@
+var str = "string";
+
+var life = 100;
+life -= 10;
+
+//boolean
+// const never changes, cant repeat
+// modern js, use let = for variables, not var anymore
+
+console.log(str);
+console.log(life);
